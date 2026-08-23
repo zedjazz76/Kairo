a93027a1a12aefb0730ab5d7e8314cfcdae304fa
zedjazz76 <zedjazz76@gmail.com>
2026-08-23 16:14:46 -0500

    test: isolate relock timeout red

--- apps/android/src/androidTest/kotlin/kairo/android/AppRelockTimeoutTest.kt
+++ apps/android/src/androidTest/kotlin/kairo/android/AppRelockTimeoutTest.kt
@@ -1,5 +1,6 @@
 package kairo.android
 
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
